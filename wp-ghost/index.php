dd7137f7bd23be39af86e581b94b4771b4d3bf6d
Alex Shive <darknight@gmail.com>
2013-07-13 16:03:13 -0700

    Updated git-ignore to only ignore the wordpress index.php file

---
+++ wp-ghost/index.php
@@ -0,0 +1,26 @@
+<?php
+require('../wp-blog-header.php');
+$redirect = $_SERVER['PHP_SELF'];
+if(!$current_user->data) header('Location: ' . wp_login_url( $redirect ));
+?><!DOCTYPE html>
+<html>
+  <head>
+    <title>WPGhost</title>
+    <meta charset="utf-8">
+    <link href="css/screen.css" media="screen, projection" rel="stylesheet" type="text/css" />
+    <link href="css/print.css" media="print" rel="stylesheet" type="text/css" />
+    <!--[if IE]>
+        <link href="css/ie.css" media="screen, projection" rel="stylesheet" type="text/css" />
+    <![endif]-->
+    <script src="js/jquery-1.8.2.min.js"></script>
+  </head>
+  <body>
+    <?php
+    if($_GET['edit'] || $_GET['new']):
+      include "editor/edit.php";
+    else:
+      include "editor/posts.php";
+    endif;
+      ?>
+  </body>
+</html>
